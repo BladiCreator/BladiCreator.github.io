ef56363e262cf6d75c1b8ad8bbb06ce536c0a2a4
Vladimir Almonte <bladicreator@gmail.com>
2023-02-09 01:44:45 +0100

    New Big Update
    A lot of new change and MapFlip "App web".
    System map redesigned, eliminated zoom control.
    Emulation of event system.
    Some new responsive adaptation

--- scripts/map.js
+++ scripts/map.js
@@ -1,33 +1,133 @@
-let latitud = 51.505;
+/*URL para la documentacion de Leaflet https://leafletjs.com/reference.html#map-property*/
+let latitud;
 let longitud = -0.09;
 let altura = 15;
-
-
 
 var latlng;
 
 if (navigator.geolocation) {
-    var success = function (position) {
-        latIng = L.latLng(position.coords.latitude, position.coords.longitude);
-        //imprimir por consola
-        console.log(position.coords.latitude)
-        console.log(position.coords.longitude)
-        //Obtener las coordenada actual del dispositivo
-        latitud = position.coords.latitude;
-        longitud = position.coords.longitude;
+  var success = function (position) {
+    latIng = L.latLng(position.coords.latitude, position.coords.longitude);
+    //imprimir por consola
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+    //Obtener las coordenada actual del dispositivo
+    latitud = parseFloat(position.coords.latitude);
+    longitud = parseFloat(position.coords.longitude);
 
-        //Instancio el objeto map
-        var map = L.map("map").setView(
-            [latitud, longitud],
-            altura
-        );
-
-        L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-            maxZoom: 19,
-            attribution: "© OpenStreetMap",
-        }).addTo(map);
-    };
-    navigator.geolocation.getCurrentPosition(success, function (msg) {
-        console.error(msg);
+    //Instancio el objeto map
+    var map = L.map("map", {
+      center: [latitud, longitud],
+      zoom: altura,
+      zoomControl: false,
     });
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution: "© OpenStreetMap",
+    }).addTo(map);
+
+    //TODO: Crear un punto donde está el usuario
+    //TODO: Crear un filtro que muestre los eventos desde un determinado rango
+
+    const intervalID = setInterval(cargadorDeEventos, 10000);
+
+    //Muestra los primeros eventos al inicio
+    simularEventos(map);
+
+    function cargadorDeEventos() {
+      //Elimina los eventos cada vez que se reinicia(Limpia el mapa)
+      clearMap(map);
+
+      simularEventos(map);
+    }
+
+    var markerOption = {
+      title: "Patinaje",
+      opacity: 0.8,
+    };
+
+    L.marker([latitud, longitud], markerOption).addTo(map);
+  };
+  navigator.geolocation.getCurrentPosition(success, function (msg) {
+    console.error(msg);
+  });
+}
+
+//TODO:Se eliminará el parametro colors y en vez de crear un circulo creará un icono
+function crearEvento(latitud, longitud, radiu, colors, map) {
+  L.circle([latitud, longitud, 1], {
+    radius: radiu,
+    color: colors,
+    fillOpacity: 0.6,
+  }).addTo(map);
+}
+
+function eliminarEvento() {}
+
+function range(valor, valorMenor, valorMayor) {
+  return valor >= valorMenor && valor <= valorMayor;
+}
+
+function simularEventos(map) {
+  for (let cantidadEventos = 0; cantidadEventos < 10000; cantidadEventos++) {
+    let numRandomLat =
+      (Math.random() / 5) * (Math.round(Math.random()) * 2 - 1);
+    let numRandomLong =
+      (Math.random() / 5) * (Math.round(Math.random()) * 2 - 1);
+
+    let latitudRandomTotal = latitud + numRandomLat;
+    let longitudRandomTotal = longitud + numRandomLong;
+    let maxDistance_KM = 5;
+    let eventDistance_KM = globalDistanceCalculator(
+      latitud,
+      longitud,
+      latitudRandomTotal,
+      longitudRandomTotal
+    );
+    //Muestra el evento si cumple la distancia que el usuario halla puesto
+    if (eventDistance_KM <= maxDistance_KM) {
+      crearEvento(
+        latitudRandomTotal,
+        longitudRandomTotal,
+        10,
+        "#FF0E0E94",
+        map
+      );
+    }
+  }
+}
+
+function clearMap(map) {
+  for (i in map._layers) {
+    if (map._layers[i]._path != undefined) {
+      try {
+        map.removeLayer(map._layers[i]);
+      } catch (e) {
+        console.log("problem with " + e + map._layers[i]);
+      }
+    }
+  }
+}
+
+function degreeToRadians(grados) {
+  return (grados * Math.PI) / 180;
+}
+
+//Formula para calcular la distancia entre 2 vectores con latitud y longitud (Formula del Haversine)
+function globalDistanceCalculator(lat1, lon1, lat2, lon2) {
+  lat1 = degreeToRadians(lat1);
+  lon1 = degreeToRadians(lon1);
+  lat2 = degreeToRadians(lat2);
+  lon2 = degreeToRadians(lon2);
+  const RADIO_TIERRA_EN_KILOMETROS = 6371;
+  let diferenciaEntreLongitudes = lon2 - lon1;
+  let diferenciaEntreLatitudes = lat2 - lat1;
+  let a =
+    Math.pow(Math.sin(diferenciaEntreLatitudes / 2.0), 2) +
+    Math.cos(lat1) *
+      Math.cos(lat2) *
+      Math.pow(Math.sin(diferenciaEntreLongitudes / 2.0), 2);
+  let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return RADIO_TIERRA_EN_KILOMETROS * c;
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,47 +1,61 @@
-
 /*Parte modo oscuro*/
-const r = document.querySelector(':root');//Obtenemos el elemento root
-const checkBoxVar = document.querySelector('input[class="Dark_Mode_Button"]');//Obtenemos el elemento input mediante el nombre de la clase
+const r = document.querySelector(":root"); //Obtenemos el elemento root
+const checkBoxVar = document.querySelector(".Dark_Mode_Button"); //Obtenemos el elemento input mediante el nombre de la clase
 
-let matched = window.matchMedia('(prefers-color-scheme: dark)').matches;//Detectamos si el navegador esta en modo oscuro
+let matched = window.matchMedia("(prefers-color-scheme: dark)").matches; //Detectamos si el navegador esta en modo oscuro
+
+//Activa el Modo oscuro ejecutando la funcion automaticamente
+activeDarkMode();
 
 function darkMode() {
-    if(checkBoxVar.checked){//Modo Oscuro
-        r.style.setProperty('--BackColor', 'rgb(20, 20, 20)');
-        r.style.setProperty('--TextColor', 'white');
-        r.style.setProperty('--MediunBackColor', 'rgb(60, 60, 60)');
-        r.style.setProperty('--buttonColor', 'rgb(40, 40, 40)');
-        r.style.setProperty('--buttonColorHover', 'rgb(80, 80, 80)');
-    }else {//Modo Normal
-        r.style.setProperty('--BackColor', 'white');
-        r.style.setProperty('--TextColor', 'black');
-        r.style.setProperty('--MediunBackColor', 'rgb(150, 150, 150)');
-        r.style.setProperty('--buttonColor', 'rgb(210, 210, 210)');
-        r.style.setProperty('--buttonColorHover', 'rgb(190, 190, 190)');
-    }
+  if (checkBoxVar.checked) {
+    //Modo Oscuro
+    r.style.setProperty("--BackColor", "rgb(20, 20, 20)");
+    r.style.setProperty("--TextColor", "white");
+    r.style.setProperty("--MediunBackColor", "rgb(60, 60, 60)");
+    r.style.setProperty("--buttonColor", "rgb(40, 40, 40)");
+    r.style.setProperty("--buttonColorHover", "rgb(80, 80, 80)");
+  } else {
+    //Modo Normal
+    r.style.setProperty("--BackColor", "white");
+    r.style.setProperty("--TextColor", "black");
+    r.style.setProperty("--MediunBackColor", "rgb(150, 150, 150)");
+    r.style.setProperty("--buttonColor", "rgb(210, 210, 210)");
+    r.style.setProperty("--buttonColorHover", "rgb(190, 190, 190)");
+  }
 }
 
-function activeDarkMode(){
-	if(matched){//Verifica si el navegador tiene el modo oscuro activado
-        checkBoxVar.checked=true;//Activa el modo oscuro
-        darkMode();
-	}
+function activarModoOscuroManual() {
+  localStorage.setItem("modoOscuroManual", checkBoxVar.checked);
+}
+
+function activeDarkMode() {
+  let modoOscuroManual = localStorage.getItem("modoOscuroManual");
+  if (modoOscuroManual == null) {
+    if (matched) {
+      //Verifica si el navegador tiene el modo oscuro activado
+      checkBoxVar.checked = true; //Activa el modo oscuro
+    }
+  } else {
+    checkBoxVar.checked = modoOscuroManual == "true" ? true : false; //Activa/Desactiva el modo oscuro
+  }
+  darkMode();
 }
 
 /*Parte barra de navegacion*/
 
-const botonMenu = document.querySelector('.iconbar');
-const botonMenuIcono = document.querySelector('.iconbar i');
-const navMenu = document.querySelector('.nav-ul');
+const botonMenu = document.querySelector(".iconbar");
+const botonMenuIcono = document.querySelector(".iconbar i");
+const navMenu = document.querySelector(".nav-ul");
 
-botonMenu.addEventListener('click', deplegarMenu);
+botonMenu.addEventListener("click", deplegarMenu);
 
-function deplegarMenu(){
-    navMenu.classList.toggle("nav-ul");
+function deplegarMenu() {
+  navMenu.classList.toggle("nav-ul");
 
-    if(!navMenu.classList.contains('nav-ul')){
-        botonMenuIcono.setAttribute('class', 'fas fa-times');
-    }else{
-        botonMenuIcono.setAttribute('class', 'fas fa-bars');
-    }
-};
+  if (!navMenu.classList.contains("nav-ul")) {
+    botonMenuIcono.setAttribute("class", "fas fa-times");
+  } else {
+    botonMenuIcono.setAttribute("class", "fas fa-bars");
+  }
+}
